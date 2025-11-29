6336d81319d255592440f572b3259e8d19fdc662
reshminagle17 <reshminagle876@gmail.com>
2025-11-29 13:30:27 +0530

    Initial commit: Email Spam Classifier project

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+ps=PorterStemmer()
+
+
+def transform_text(text):
+    # lower case
+    text = text.lower()
+
+    # Tokenization
+    text = nltk.word_tokenize(text)
+
+    # Removing special characters
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    # Removing stop words and punctuation
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    # stemming
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+vectorizer = pickle.load(open("vectorizer.pkl", 'rb'))
+model=pickle.load(open('model.pkl','rb'))
+
+st.title('Email/SMS Spam Classifier')
+input_sms=st.text_area("Enter the Messages")
+if st.button('Predict'):
+    #1. preprocess
+    transform_sms=transform_text(input_sms)
+    # 2. vectorizer
+    vector_input = vectorizer.transform([transform_sms])
+
+    #3. predict
+    result=model.predict(vector_input)[0]
+    # 4.display
+    if result==1:
+        st.header("spam")
+    else:
+        st.header('not spam')
+
+
+
